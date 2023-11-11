9e1d7b43f1485927e1a4b1278cb0c7b5c593caf8
Kunal Keshan <kunalkeshan12@gmail.com>
2023-11-11 19:16:35 +0530

    chore: mapped out domains to domains section

--- components/cards/DomainCard.tsx
+++ components/cards/DomainCard.tsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import Image from 'next/image';
 
-const DomainCard = () => {
-	return <div>DomainCard</div>;
+type DomainCardProps = React.ComponentProps<'li'> & {
+	domain: Domain;
+};
+
+const DomainCard: React.FC<DomainCardProps> = ({ domain }) => {
+	return (
+		<li className='flex flex-col gap-6 w-72 text-white h-full self-stretch'>
+			<div className='p-3 rounded-lg bg-app-primary/20'>
+				<Image
+					src={domain.image}
+					alt='domain'
+					width={500}
+					height={600}
+					objectFit='cover'
+				/>
+			</div>
+			<div className='flex flex-col items-center text-center gap-2'>
+				<h3 className='text-base md:text-lg lg:text-xl mt-2 font-medium'>
+					{domain.name}
+				</h3>
+				<p className='text-justify'>{domain.description}</p>
+			</div>
+		</li>
+	);
 };
 
 export default DomainCard;
--- components/home/Domains.tsx
+++ components/home/Domains.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import DomainCard from '../cards/DomainCard';
 
 type DomainsProps = React.ComponentProps<'section'> & {
 	domains: Domain[];
 };
 
 const Domains: React.FC<DomainsProps> = ({ domains }) => {
-	return <div>Domains</div>;
+	return (
+		<section className='w-full max-w-7xl mx-auto flex items-center justify-center flex-col p-4 md:p-16'>
+			<h2 className='uppercase text-xl md:text-2xl lg:text-4xl leading-tight font-bold'>
+				Domains
+			</h2>
+			<ul className='flex w-full items-center justify-evenly flex-wrap gap-8 mt-8'>
+				{domains.map((domain) => (
+					<DomainCard
+						key={`landing-domain-${domain._id}`}
+						domain={domain}
+					/>
+				))}
+			</ul>
+		</section>
+	);
 };
 
 export default Domains;
