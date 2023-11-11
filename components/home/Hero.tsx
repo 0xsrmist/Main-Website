afb532504ca75c5217545b711725414bb42a23ce
Kunal Keshan <kunalkeshan12@gmail.com>
2023-11-11 18:41:40 +0530

    chore: updated view for hero

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -3,17 +3,29 @@ import Image from 'next/image';
 
 const Hero = () => {
 	return (
-		<section className='min-h-screen bg-[url(/assets/images/hero.webp)] overflow-hidden bg-no-repeat bg-cover bg-center relative w-full flex pt-32 md:pt-0 items-start justify-center'>
+		<section className='min-h-screen relative w-full flex items-start justify-center overflow-hidden z-10'>
 			<div
-				className='absolute w-full h-full top-0 left-0 z-10'
-				id='home__hero'
+				className='absolute w-full h-full top-0 left-0 z-[5]'
+				// id='home__hero'
 			>
 				<div id='stars' />
 				<div id='stars2' />
 				<div id='stars3' />
 				<div id='stars4' />
+				<div className='w-full h-full'>
+					<Image
+						src='/assets/images/hero.webp'
+						alt=''
+						width={100}
+						height={100}
+						className='w-full h-auto object-cover aspect-[9/16] md:aspect-auto xl:-translate-y-60'
+						priority
+						unoptimized
+						// sizes='(max-width: 640px) 100vw, (max-width: 1440px) 100vw, 2560px'
+					/>
+				</div>
 			</div>
-			<div className='w-full max-w-7xl flex items-center justify-center text-center flex-col h-fit p-4 md:p-16 z-20'>
+			<div className='w-full max-w-7xl flex items-center justify-center text-center flex-col h-fit p-4 md:p-16 z-10'>
 				<div className='w-8 md:w-16'>
 					<Image
 						src='/assets/images/diamond.png'
