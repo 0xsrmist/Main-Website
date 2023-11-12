d761a9d399dd427de7a5ba8bddf913e6602005a7
Kunal Keshan <kunalkeshan12@gmail.com>
2023-11-12 17:18:53 +0530

    chore: added legal data

--- constants/tph.ts
+++ constants/tph.ts
@@ -43,3 +43,108 @@ export const TPH_SOCIALS: SocialsList = [
 
 export const TPH_MAIN_WEBSITE_GITHUB_REPO =
 	'https://github.com/0xsrmist/Main-Website/';
+
+export const TPH_TERMS_AND_CONDITIONS: TermsAndConditionsData = [
+	{
+		title: 'Intellectual Property Rights',
+		descriptions: [
+			'Other than the content you own, under these terms, TPHxSRMIST and/or its licensors own all the intellectual property rights and materials contained on this website.',
+			'You are granted limited license only for purposes of viewing the material contained on this website.',
+		],
+	},
+	{
+		title: 'Restrictions',
+		descriptions: [
+			'You are specifically restricted from all of the following:',
+			'1. Publishing any website material in any other media.',
+			'2. Selling, sublicensing, and/or otherwise commercializing any website material.',
+			'3. Publicly performing and/or showing any website material without acknowledgment.',
+			'4. Using this website in a way that is damaging or could be damaging to this website.',
+			'5. Using this website contrary to applicable laws and regulations, or in a way that causes, or may cause, harm to the website, or to any person or business entity.',
+		],
+	},
+	{
+		title: 'Your Content',
+		descriptions: [
+			'In these website standard terms and conditions, "Your Content" shall mean any audio, video, text, images, or other material you choose to display on this website. By displaying Your Content, you grant TPHxSRMIST a non-exclusive, worldwide irrevocable, sub licensable license to use, reproduce, adapt, publish, translate, and distribute it in any and all media.',
+			'Your Content must be your own and must not be invading any third-party rights. TPHxSRMIST reserves the right to remove any of Your Content from this website at any time without notice.',
+		],
+	},
+	{
+		title: 'No warranties',
+		descriptions: [
+			'This website is provided "as is," with all faults, and TPHxSRMIST expresses no representations or warranties, of any kind related to this website or the materials contained on this website. Also, nothing contained on this website shall be interpreted as advising you.',
+		],
+	},
+	{
+		title: 'Limitation of liability',
+		descriptions: [
+			'In no event shall TPHxSRMIST, nor any of its officers, directors, and employees, be held liable for anything arising out of or in any way connected with your use of this website, whether such liability is under contract. TPHxSRMIST, including its officers, directors, and employees, shall not be held liable for any indirect, consequential, or special liability arising out of or in any way related to your use of this website.',
+		],
+	},
+	{
+		title: 'Indemnification',
+		descriptions: [
+			'You hereby indemnify to the fullest extent TPHxSRMIST from and against any and/or all liabilities, costs, demands, causes of action, damages, and expenses arising in any way related to your breach of any of the provisions of these terms.',
+		],
+	},
+];
+
+export const TPH_PRIVACY_POLICY: PrivacyPolicyData = [
+	{
+		title: 'Information We Collect',
+		descriptions: [
+			'We may collect personal identification information from users in various ways, including but not limited to, when users visit our site, register on the site, fill out a form, and in connection with other activities, services, features, or resources we make available on our site.',
+			'Users may be asked for, as appropriate, name, email address, phone number, and other relevant information.',
+			'We will collect personal identification information from users only if they voluntarily submit such information to us. Users can always refuse to supply personal identification information, except that it may prevent them from engaging in certain site-related activities.',
+		],
+	},
+	{
+		title: 'How We Use Collected Information',
+		descriptions: [
+			'TPHxSRMIST may collect and use users’ personal information for the following purposes:',
+			'1. To personalize user experience: We may use information in the aggregate to understand how our users as a group use the services and resources provided on our site.',
+			'2. To improve our site: We continually strive to improve our website offerings based on the information and feedback we receive from you.',
+			'3. To send periodic emails: We may use the email address to respond to inquiries, questions, and/or other requests.',
+		],
+	},
+	{
+		title: 'How We Protect Your Information',
+		descriptions: [
+			'We adopt appropriate data collection, storage, and processing practices and security measures to protect against unauthorized access, alteration, disclosure, or destruction of your personal information, username, password, transaction information, and data stored on our site.',
+			'Sensitive and private data exchange between the site and its users happens over an SSL-secured communication channel and is encrypted and protected with digital signatures.',
+		],
+	},
+	{
+		title: 'Sharing Your Personal Information',
+		descriptions: [
+			'We do not sell, trade, or rent users’ personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates, and advertisers.',
+		],
+	},
+	{
+		title: 'Data Security',
+		descriptions: [
+			'We take appropriate measures to protect your personal information from unauthorized access, disclosure, alteration, or destruction. Our website employs industry-standard security protocols to safeguard your data.',
+		],
+	},
+	{
+		title: 'Third-Party Links',
+		descriptions: [
+			'Our website may contain links to third-party websites. Please note that we do not control the privacy practices of these external websites. We encourage you to review the privacy policies of any website you visit via our links.',
+		],
+	},
+	{
+		title: 'Changes to Privacy Policy',
+		descriptions: [
+			'We may update our Privacy Policy to reflect changes in our practices or legal requirements. Any updates will be posted on this page, and the effective date will be modified accordingly.',
+		],
+	},
+	{
+		title: 'Contact Us',
+		descriptions: [
+			`If you have any questions or concerns regarding our Privacy Policy, please contact us at ${TPH_EMAIL} or ${TPH_CONTACT_NUMBERS.join(
+				', '
+			)}.`,
+		],
+	},
+];
