5a6d51b53d311dd1712e4dcb27ee0a017ee503c8
Kunal Keshan <kunalkeshan12@gmail.com>
2023-11-12 10:30:35 +0530

    Feat/navbar (#32)
    * chore: desktop nav
    
    * chore: added responsive nav

--- layouts/Navbar.tsx
+++ layouts/Navbar.tsx
@@ -3,6 +3,9 @@
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { NAVBAR_NAVIGATION } from '@/constants/navigation';
+import { Button } from '@/components/ui/button';
+import SheetNav from './SheetNav';
 
 const Navbar = () => {
 	const [header, setHeader] = useState('bg-transparent');
@@ -20,7 +23,7 @@ const Navbar = () => {
 		<header
 			className={`${header} w-full z-50 transition-all duration-300 md:px-16 p-4`}
 		>
-			<div className='w-full max-w-7xl mx-auto'>
+			<div className='w-full max-w-7xl mx-auto flex items-center justify-between'>
 				<Link href='/' className='w-28 md:w-40 block'>
 					<Image
 						src='/assets/images/logo.png'
@@ -31,6 +34,29 @@ const Navbar = () => {
 						unoptimized
 					/>
 				</Link>
+				<ul className='hidden md:flex items-center gap-8'>
+					{NAVBAR_NAVIGATION.map((item) => (
+						<li key={`navbar-nav-${item.url}`}>
+							<Link
+								href={item.url}
+								target={item.target}
+								className='font-medium text-lg hover:text-app-primary transition-all duration-300'
+							>
+								{item.name}
+							</Link>
+						</li>
+					))}
+					<li>
+						<Button
+							className='font-semibold rounded-2xl'
+							variant={'secondary'}
+							asChild
+						>
+							<Link href={'/contact'}>Collab with us</Link>
+						</Button>
+					</li>
+				</ul>
+				<SheetNav />
 			</div>
 		</header>
 	);
---
+++ layouts/SheetNav.tsx
@@ -0,0 +1,69 @@
+'use client';
+
+import React, { useState } from 'react';
+import {
+	Sheet,
+	SheetContent,
+	SheetDescription,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
+} from '@/components/ui/sheet';
+import { Menu } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { NAVBAR_NAVIGATION } from '@/constants/navigation';
+import Link from 'next/link';
+
+const SheetNav = () => {
+	const [open, setOpen] = useState(false);
+
+	const handleCloseSheet = () => {
+		setOpen(false);
+	};
+
+	return (
+		<Sheet open={open} onOpenChange={setOpen}>
+			<SheetTrigger asChild className='flex md:hidden'>
+				<Button
+					variant={'secondary'}
+					size={'icon'}
+					className='font-semibold'
+				>
+					<Menu />
+				</Button>
+			</SheetTrigger>
+			<SheetContent side={'top'} className='block md:hidden'>
+				<SheetHeader>
+					<SheetTitle className='text-center'>
+						TPH X SRMIST
+					</SheetTitle>
+					<SheetDescription className='flex flex-col items-center justify-center'>
+						<ul className='flex items-center gap-8 flex-wrap justify-center'>
+							{NAVBAR_NAVIGATION.map((item) => (
+								<li key={`navbar-nav-${item.url}`}>
+									<Link
+										href={item.url}
+										target={item.target}
+										className='font-medium text-lg hover:text-app-primary transition-all duration-300'
+										onClick={handleCloseSheet}
+									>
+										{item.name}
+									</Link>
+								</li>
+							))}
+						</ul>
+						<Button
+							className='font-semibold rounded-2xl mt-4'
+							onClick={handleCloseSheet}
+							asChild
+						>
+							<Link href={'/contact'}>Collab with us</Link>
+						</Button>
+					</SheetDescription>
+				</SheetHeader>
+			</SheetContent>
+		</Sheet>
+	);
+};
+
+export default SheetNav;
