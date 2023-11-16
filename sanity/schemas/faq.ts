f56f4539907b7666208202fdf36d6b11e39502b1
Kunal Keshan <kunalkeshan12@gmail.com>
2023-11-16 14:20:43 +0530

    chore: updated faq schema

--- sanity/schemas/faq.ts
+++ sanity/schemas/faq.ts
@@ -18,5 +18,39 @@ export default defineType({
 			description:
 				'The detailed answer to the frequently asked question.',
 		}),
+		defineField({
+			name: 'order',
+			title: 'Priority Order',
+			description:
+				'A numerical value representing the priority or order of this Q/A. Lower values indicate higher priority. Use this field to customize the order in which Q/A are displayed or sorted.',
+			type: 'number',
+			validation: (Rule) => Rule.required(),
+		}),
 	],
+	orderings: [
+		{
+			title: 'Priority Order, Asc',
+			name: 'orderAsc',
+			by: [{ field: 'order', direction: 'asc' }],
+		},
+		{
+			title: 'Priority Order, Desc',
+			name: 'orderDesc',
+			by: [{ field: 'order', direction: 'desc' }],
+		},
+	],
+	preview: {
+		select: {
+			question: 'question',
+			order: 'order',
+		},
+		prepare(selection) {
+			const { question, order } = selection;
+			return {
+				title: `${order ? `${order}.` : null} ${
+					question ? question : null
+				}`,
+			};
+		},
+	},
 });
