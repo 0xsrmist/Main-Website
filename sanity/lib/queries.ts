24af0a52a8e4393b0adb8a95ca2ca2ce349a6197
kunalkeshan <kunalkeshan12@gmail.com>
2023-12-07 03:23:10 +0530

    chore: optimized queries

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -4,27 +4,27 @@ import { groq } from 'next-sanity';
 
 // Get all posts
 export const postsQuery = groq`*[_type == "post"]{
-  _id, title, "slug": slug.current, "image": mainImage.asset->url, "alt": mainImage.alt, categories[]->{title, "slug": slug.current}, publishedAt, description, author->
+  ..., "slug": slug.current, "image": mainImage.asset->url, "alt": mainImage.alt, categories[]->{..., "slug": slug.current}, author->
 } | order(publishedAt desc) | order(featured asc)`;
 
 // Get all featured posts
 export const featuredPostsQuery = groq`*[_type == "post" && featured == true]{
-_id, title, "slug": slug.current, "image": mainImage.asset->url, "alt": mainImage.alt, categories[]->{title, "slug": slug.current}, description, publishedAt, author->
+..., "slug": slug.current, "image": mainImage.asset->url, "alt": mainImage.alt, categories[]->{..., "slug": slug.current}, author->
 } | order(publishedAt desc)`;
 
 // Get a single post by its slug
 export const postQuery = groq`*[_type == "post" && slug.current == $slug][0]{ 
-  _id, "slug": slug.current, title, "image": mainImage.asset->url, "alt": mainImage.alt, body, author->{"image": image.asset->url, "alt": image.alt, name, "slug": slug.current}, categories[]->{title, "slug": slug.current}, publishedAt, description
+  ..., "slug": slug.current, "image": mainImage.asset->url, "alt": mainImage.alt, body, author->{"image": image.asset->url, "alt": image.alt, name, "slug": slug.current}, categories[]->{title, "slug": slug.current}
 }`;
 
 // Get a single event by its slug
-export const eventQeury = groq`*[_type == "event" && slug.current == $slug][0]{
-  _id, "slug": slug.current, title, about, description, eventType, description, mode, location, address, startDate, endDate, url, "coverImage": coverImage.asset->url, "coverImageAlt": coverImage.alt, "imageGallery": imageGallery[]{"image": image.asset->url, "alt": image.alt, caption }, "speakers": speakers[]{name, role, "image": image.asset->url, "alt": image.alt, url}, (startDate <= now() && endDate >= now()) => {"status": "ongoing"}, (startDate >= now() && endDate >= now()) => {"status": "upcoming"}, (startDate <= now() && endDate <= now()) => {"status": "completed"}
+export const eventQeury = groq`*[_type == "event" && eventType != "recruitment" && slug.current == $slug][0]{
+  ..., "slug": slug.current, "coverImage": coverImage.asset->url, "coverImageAlt": coverImage.alt, "imageGallery": imageGallery[]{..., "image": image.asset->url, "alt": image.alt }, "speakers": speakers[]{..., "image": image.asset->url, "alt": image.alt}, (startDate <= now() && endDate >= now()) => {"status": "ongoing"}, (startDate >= now() && endDate >= now()) => {"status": "upcoming"}, (startDate <= now() && endDate <= now()) => {"status": "completed"}
 }`;
 
 // Get a single team member by its lug
 export const teamMemberQuery = groq`*[_type == "team" && slug.current == $slug][0]{
-  ..., _id, "slug": slug.current, "image": image.asset->url, "alt": image.alt, "domain": domain->{name, description, "slug": slug.current, "image": image.asset->url, "alt": image.alt}, "role": role->{name, "slug": slug.current, description}
+  ..., "slug": slug.current, "image": image.asset->url, "alt": image.alt, "domain": domain->{..., "slug": slug.current, "image": image.asset->url, "alt": image.alt}, "role": role->{..., "slug": slug.current}
 }`;
 
 // Get all post slugs
@@ -33,7 +33,12 @@ export const postPathsQuery = groq`*[_type == "post" && defined(slug.current)][]
 }`;
 
 // Get all event slugs
-export const eventPathsQuery = groq`*[_type == "event" && defined(slug.current)][]{
+export const eventPathsQuery = groq`*[_type == "event" && eventType != "recruitment" && defined(slug.current)][]{
+  "params": { "slug": slug.current }
+}`;
+
+// Get all recruitment slugs
+export const recruitmentPathsQuery = groq`*[_type == "event" && eventType == "recruitment" && defined(slug.current)][]{
   "params": { "slug": slug.current }
 }`;
 
@@ -44,39 +49,41 @@ export const teamMemberPathsQuery = groq`*[_type == "team" && defined(slug.curre
 
 // Get all team members
 export const teamMembersQuery = groq`*[_type == "team" && currentlyWorking == true]{
-  ..., _id, "slug": slug.current, "image": image.asset->url, "alt": image.alt, "domain": domain->{name, description, "slug": slug.current}, "role": role->{name, "slug": slug.current, description}
+  ..., "slug": slug.current, "image": image.asset->url, "alt": image.alt, "domain": domain->{..., "slug": slug.current}, "role": role->{..., "slug": slug.current}
 } | order(role->order desc)`;
+
 // Get all domains
 export const domainsQuery = groq`*[_type == "domain"]{
-  _id, "slug": slug.current, name, "image": image.asset->url, "alt": image.alt, description
+  ..., "slug": slug.current, "image": image.asset->url, "alt": image.alt
 }`;
 
 // Get all partners
 export const partnersQuery = groq`*[_type == "partner"]{
-  _id, name, "image": image.asset->url, "alt": image.alt, description, website
+  ..., "image": image.asset->url, "alt": image.alt
 }`;
 
 // Get all active notifications
-export const activeNotificationsQuery = groq`*[_type == "notification" && startDate <= now() && endDate >= now()]{
-  _id, title, description, url, startDate, endDate
-}`;
+export const activeNotificationsQuery = groq`*[_type == "notification" && startDate <= now() && endDate >= now()]{ ... }`;
 
 // Get latest recruitment event
 export const latestRecruitmentEventQuery = groq`*[_type == "event" && eventType == "recruitment" && startDate <= now() && endDate >= now()]{
-  _id, "slug": slug.current, title, about, description, eventType, mode, location, address, startDate, endDate, url, "coverImage": coverImage.asset->url, "coverImageAlt": coverImage.alt, (startDate <= now() && endDate >= now()) => {"status": "ongoing"}, (startDate >= now() && endDate >= now()) => {"status": "upcoming"}, (startDate <= now() && endDate <= now()) => {"status": "completed"}
+  ..., "slug": slug.current, "coverImage": coverImage.asset->url, "coverImageAlt": coverImage.alt, (startDate <= now() && endDate >= now()) => {"status": "ongoing"}, (startDate >= now() && endDate >= now()) => {"status": "upcoming"}, (startDate <= now() && endDate <= now()) => {"status": "completed"}
 } | order(startDate asc) [0]`;
+
+// Get all completed recruitments
+export const pastRecruitmentEventsQuery = groq`*[_type == "event" && eventType == "recruitment" && startDate >= now() && endDate >= now()]{
+  ..., "slug": slug.current, "coverImage": coverImage.asset->url, "coverImageAlt": coverImage.alt, (startDate <= now() && endDate >= now()) => {"status": "ongoing"}, (startDate >= now() && endDate >= now()) => {"status": "upcoming"}, (startDate <= now() && endDate <= now()) => {"status": "completed"}
+} | order(endDate desc)`;
 
 // Get all events
 export const eventsQuery = groq`*[_type == "event" && eventType != "recruitment"]{
-  _id, "slug": slug.current, title, about, description, eventType, description, mode, location, address, startDate, endDate, url, "coverImage": coverImage.asset->url, "coverImageAlt": coverImage.alt, (startDate <= now() && endDate >= now()) => {"status": "ongoing"}, (startDate >= now() && endDate >= now()) => {"status": "upcoming"}, (startDate <= now() && endDate <= now()) => {"status": "completed"}
+  ..., "slug": slug.current, "coverImage": coverImage.asset->url, "coverImageAlt": coverImage.alt, (startDate <= now() && endDate >= now()) => {"status": "ongoing"}, (startDate >= now() && endDate >= now()) => {"status": "upcoming"}, (startDate <= now() && endDate <= now()) => {"status": "completed"}
 } | order(endDate desc)`;
 
 // Get all latest events
 export const latestEventsQuery = groq`*[_type == "event" && eventType != "recruitment"]{
-  _id, "slug": slug.current, title, about, description, eventType, description, mode, location, address, startDate, endDate, url, "coverImage": coverImage.asset->url, "coverImageAlt": coverImage.alt, (startDate <= now() && endDate >= now()) => {"status": "ongoing"}, (startDate >= now() && endDate >= now()) => {"status": "upcoming"}, (startDate <= now() && endDate <= now()) => {"status": "completed"}
+  ..., "slug": slug.current, "coverImage": coverImage.asset->url, "coverImageAlt": coverImage.alt, (startDate <= now() && endDate >= now()) => {"status": "ongoing"}, (startDate >= now() && endDate >= now()) => {"status": "upcoming"}, (startDate <= now() && endDate <= now()) => {"status": "completed"}
 } | order(endDate desc) [0..2]`;
 
 // Get all FAQs
-export const faqsQuery = groq`*[_type == "faq"]{
-  _id, question, answer, order
-} | order(order asc)`;
+export const faqsQuery = groq`*[_type == "faq"]{ ... } | order(order asc)`;
