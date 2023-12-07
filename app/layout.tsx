8aed836ec97d48e0add0bea5bbd90492acbc1efb
kunalkeshan <kunalkeshan12@gmail.com>
2023-12-07 08:39:22 +0530

    chore: metadata base added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,28 @@
 import { Archivo } from 'next/font/google';
 import './globals.css';
+import { Metadata } from 'next';
+import { TPH_WEBSITE_URL } from '@/constants/tph';
 
 const archivo = Archivo({ subsets: ['latin'], display: 'swap' });
+
+export const metadata: Metadata = {
+	metadataBase: new URL(TPH_WEBSITE_URL),
+	title: 'TPH x SRMIST',
+	description: 'Official Website of TPH x SRMIST.',
+	openGraph: {
+		type: 'website',
+		title: 'TPH x SRMIST | Home',
+		description: 'Official Website of TPH x SRMIST.',
+		url: `${TPH_WEBSITE_URL}`,
+	},
+	twitter: {
+		title: 'TPH x SRMIST | Home',
+		description: 'Official Website of TPH x SRMIST.',
+		card: 'summary_large_image',
+		site: '@0xsrmist',
+		creator: '@0xsrmist',
+	},
+};
 
 export default function RootLayout({
 	children,
