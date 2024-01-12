108a40ecdc815caa7a988e9be965a3a45c7fce8d
Kunal Keshan <kunalkeshan12@gmail.com>
2024-01-13 00:31:30 +0530

    chore: adjusted event grid layouts

--- components/events/List.tsx
+++ components/events/List.tsx
@@ -13,7 +13,7 @@ type EventsListProps = React.ComponentProps<'section'> & {
 const EventsList: React.FC<EventsListProps> = ({ events }) => {
 	return (
 		<section className='w-full max-w-7xl p-4 md:p-16 mx-auto' id='events'>
-			<ul className='w-full grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8'>
+			<ul className='w-full grid grid-cols-1 md:grid-cols-3 gap-8'>
 				{events.map((event) => (
 					<EventInfo
 						key={`event-page-event-info-${event._id}`}
